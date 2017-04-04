dce8f4599a4ef2d1f83f165293c5379eb75cc2c2
sudesh@sg.ibm.com <sudesh@sg.ibm.com>
2017-04-04 20:19:28 +0800

    paths specified without -i or -o; assuming --only paths...

--- app.js
+++ app.js
@@ -7,7 +7,33 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
  
-var routes = require("./routes/routes.js")(app);
+app.get("/", function(req, res) {
+    res.send("Hello Unilab ! Welcome to Manila");
+});
+
+	app.get("/account", function(req, res) {
+    var accountMock = {
+        "username": "sudesh",
+        "password": "1234",
+        "twitter": "ksudesh"
+    }
+    if(!req.query.username) {
+        return res.send({"status": "error", "message": "missing username"});
+    } else if(req.query.username != accountMock.username) {
+        return res.send({"status": "error", "message": "wrong username"});
+    } else {
+        return res.send(accountMock);
+    }
+});
+
+ app.post("/account", function(req, res) {
+    if(!req.body.username || !req.body.password || !req.body.twitter) {
+        return res.send({"status": "error", "message": "missing a parameter"});
+    } else {
+        return res.send(req.body);
+    }
+});
+ 
  
 var server = app.listen(3000, function () {
     console.log("Listening on port %s...", server.address().port);

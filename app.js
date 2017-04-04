055cf725d74b30f315590e88e9bdf4c66ca9e3a8
sudesh@sg.ibm.com <sudesh@sg.ibm.com>
2017-04-04 19:27:08 +0800

    added app.js and the rest api server

--- app.js
+++ app.js
@@ -1,28 +1,14 @@
-/*eslint-env node*/
 
-//------------------------------------------------------------------------------
-// node.js starter application for Bluemix
-//------------------------------------------------------------------------------
 
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
-var express = require('express');
-
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
-
-// create a new express server
+var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
-
-// serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
-
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
-
-// start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+ 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+ 
+var routes = require("./routes/routes.js")(app);
+ 
+var server = app.listen(3000, function () {
+    console.log("Listening on port %s...", server.address().port);
 });
